99c8c441a8a2d9d06a7aebcfe5488e3335dc3700
Vishal Amin <vishalamin200@gmail.com>
2026-01-19 23:51:25 +0530

    download module before using it

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template, current_app
+from flask import Flask
 from flask_cors import CORS
+import nltk
 
 from routes.hate_routes import hate_bp
 
 app = Flask(__name__)
 CORS(app)
 
+# Download NLTK data at startup
+try:
+    nltk.data.find("corpora/stopwords")
+except LookupError:
+    nltk.download("stopwords")
+
+try:
+    nltk.data.find("tokenizers/punkt")
+except LookupError:
+    nltk.download("punkt")
+
 app.register_blueprint(hate_bp)
 
 @app.route('/')
 def index():
-    return "Server is running successfully!" 
+    return "Server is running successfully!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
