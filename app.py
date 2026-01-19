b750be1b76e75e5a907c0057868da735f3e83033
Vishal Amin <vishalamin200@gmail.com>
2026-01-20 00:04:33 +0530

    corrected code for download the package intially

--- app.py
+++ app.py
@@ -2,11 +2,6 @@ from flask import Flask
 from flask_cors import CORS
 import nltk
 
-from routes.hate_routes import hate_bp
-
-app = Flask(__name__)
-CORS(app)
-
 # Download NLTK data at startup
 try:
     nltk.data.find("corpora/stopwords")
@@ -17,6 +12,11 @@ try:
     nltk.data.find("tokenizers/punkt")
 except LookupError:
     nltk.download("punkt")
+
+from routes.hate_routes import hate_bp
+
+app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(hate_bp)
 
